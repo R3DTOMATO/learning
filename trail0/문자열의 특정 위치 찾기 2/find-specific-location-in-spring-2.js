28b21cf74e836d028de075ad42a3e51a18b447fe
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-24 17:28:20 +0900

    [Passed] 문자열의 특정 위치 찾기 2 (64ms, 10MB)

--- trail0/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.js
+++ trail0/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.js
@@ -3,15 +3,9 @@ let input = fs.readFileSync(0).toString().trim();
 let arr = ["apple", "banana", "grape", "blueberry", "orange"];
 let cnt = 0;
 for(let i = 0 ; i<5;i++){
-    if(arr[i][2] == input){
+    if(arr[i][2]==input || arr[i][3] == input){
         console.log(arr[i])
         cnt++;
-        continue;
-    }
-    else if(arr[i][3] == input){
-        console.log(arr[i]);
-        cnt++;
-        continue;
     }
 }
 console.log(cnt);
