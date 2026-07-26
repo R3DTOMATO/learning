d46a4629dbde52a41119b51b7398c2e3ebf991b3
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-26 16:03:33 +0900

    [Passed] 정상적인 구구단 (82ms, 12MB)

---
+++ trail0/정상적인 구구단/normal-multiple.js
@@ -0,0 +1,14 @@
+const fs =require("fs");
+let input  =  fs.readFileSync(0).toString().trim();
+let N = Number(input);
+
+for(let i = 1; i <= N; i++){
+    let str = "";
+    for(let j = 1; j <= N; j++){
+        str +=`${i} * ${j} = ${i*j}`;
+        if(j!=N){
+            str +=", ";
+        }
+    }
+    console.log(str);
+}
