fec9befa2fc6937c4335b4f6d613bb350f092475
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-29 21:58:01 +0900

    [Passed] 달에서 무게 구하기 (2ms, 0MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout<<fixed;
+    cout.precision(6);
+    int a = 13;
+    float b = 0.165;
+    cout<<a<<" * "<<b<<" = "<<a*b;
+    return 0;
+}
