aec1598df2782145d3497141c46b1e20abe9169c
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-29 22:16:50 +0900

    [Passed] 길이 단위 변환하기 (2ms, 0MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout<<fixed;
+    cout.precision(1);
+
+    cout<<"9.2ft = "<<9.2*30.48<<"cm"<<endl<<"1.3mi = "<<1.3*160934<<"cm";
+    return 0;
+}
