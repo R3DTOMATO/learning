bfa2f09c10af176c7b59d8007ddaad23eaf877c2
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-29 16:16:51 +0900

    [Passed] 특정 원소들의 합 (65ms, 10MB)

---
+++ trail0/특정 원소들의 합/sum-of-specific-elements.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let arr = [];
+for(let i = 0; i<4; i++){
+    arr.push(input[i].split(" ").map(Number));
+}
+
+let result = 0;
+for(let i = 0; i < 4; i++){
+    for(let j =0; j<=i;j++){
+        result +=arr[i][j];
+    }
+}
+console.log(result);
