8cd0df8512ae7ea312ba9a3d23763b06e651b27d
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-31 08:46:50 +0900

    [Passed] 데이터 교환 (2ms, 0MB)

---
+++ trail1/데이터 교환/exchange-data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp = a;
+    a = c;
+    c = b;
+    b = temp;
+    cout<<a<<endl<<b<<endl<<c;
+    return 0;
+}
