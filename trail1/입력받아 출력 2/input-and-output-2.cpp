9c5916805a97a6c09760ab8221691ea09b5a7121
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-31 10:01:56 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int input1,input2;
+    cin>>input1;
+    cin.get();
+    cin>>input2;
+
+    cout<<input1<<input2;
+    return 0;
+}
