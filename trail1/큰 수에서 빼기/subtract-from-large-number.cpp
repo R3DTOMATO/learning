dd1c992cff0faca0bf02b820f8694e0efe6f0879
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-31 12:30:47 +0900

    [Passed] 큰 수에서 빼기 (2ms, 0MB)

---
+++ trail1/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    if(a<b){
+        int temp =a;
+        a=b;
+        b=temp;
+    }
+    cout<<a-b;
+    return 0;
+}
