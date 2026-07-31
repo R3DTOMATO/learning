306553ac8171cab669d0e64a89a9b4121929f808
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-31 09:16:58 +0900

    [Passed] 길이 단위 환산하기 (2ms, 0MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double N;
+    cin>>N;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<N*30.48;
+    return 0;
+}
