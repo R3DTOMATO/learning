28aa881538251c2a69567839f05f9168a248ce5c
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-31 12:49:41 +0900

    [Passed] 정사각형의 넓이 (1ms, 0MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+    cout<<N*N<<endl;
+    if(N<5){
+        cout<<"tiny";
+    }
+    return 0;
+}
