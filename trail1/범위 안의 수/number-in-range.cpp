6f1dd25e42556e2e26e52e10b575d244fb986cfc
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-07-31 21:49:28 +0900

    [Passed] 범위 안의 수 (2ms, 0MB)

---
+++ trail1/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    if(10<=a&&20>=a){
+        cout<<"yes";
+    }else{
+        cout<<"no";
+    }
+    return 0;
+}
