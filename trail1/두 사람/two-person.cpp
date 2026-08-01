40c45dbcd60c148d8b12e7a2690be40d8eee31f0
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-01 11:34:22 +0900

    [Passed] 두 사람 (2ms, 0MB)

---
+++ trail1/두 사람/two-person.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    char a1,b1;
+    cin>>a>>a1;
+    cin>>b>>b1;
+
+    cout<<((a>=19&&a1=='M')||(b>=19&&b1=='M'));
+    return 0;
+}
