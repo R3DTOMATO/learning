0d27804760fb86523dbea91c7a416a2573b5a3bd
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-02 09:30:22 +0900

    [Passed] 정수 입력받아 배수 출력 (2ms, 0MB)

---
+++ trail1/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    for(int i=1;i<=5;i++){
+        cout<<a*i<<" ";
+    }
+    return 0;
+}
