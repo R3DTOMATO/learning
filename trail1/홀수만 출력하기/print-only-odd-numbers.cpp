f374a92faf05b821aba98fe13da5d9bc6af7d781
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-05 21:13:31 +0900

    [Passed] 홀수만 출력하기 (2ms, 0MB)

---
+++ trail1/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,a;
+    cin>>n;
+    for(int i = 0; i<n;i++){
+        cin>>a;
+        if(a%3==0 && a%2!=0){
+            cout<<a<<endl;
+        }
+    }
+    return 0;
+}
