2e5a266d9f06f3591ab2dd44ba9b60ad3d9734fe
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-05 13:33:13 +0900

    [Passed] N까지 자연수 출력 (2ms, 0MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i;
+    cin>>i;
+    int a = 1;
+    while(a<=i){
+        cout<<a<<" ";
+        a++;
+    }
+    return 0;
+}
