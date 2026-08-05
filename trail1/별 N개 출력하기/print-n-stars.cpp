4f1cf993ee61b7c827985ef6a943b0ba36e6e196
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-05 14:02:56 +0900

    [Passed] 별 N개 출력하기 (2ms, 0MB)

---
+++ trail1/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,i=1;
+    cin>>n;
+    while(i<=n){
+        cout<<"*"<<endl;
+        i++;
+    }
+    return 0;
+}
