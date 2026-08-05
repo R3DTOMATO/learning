85f11b5480f53fa00923a7a41d679e8455bc8b6b
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-05 21:09:20 +0900

    [Passed] 369 게임 (2ms, 0MB)

--- trail1/369 게임/369-game.cpp
+++ trail1/369 게임/369-game.cpp
@@ -4,29 +4,41 @@ using namespace std;
 int main() {
     // Please write your code here.
     int n;
-    cin>>n;
-    int i =1;
-    while(i<=n){
-        if(i/10 != 0){
-            if(i/10 == 3 || i/10 == 6||i/10 == 9){
-                cout<<0<<" ";
-            }else{
-                int tmp = i%10;
-                if(tmp == 3 || tmp == 6 || tmp == 9 || i%3==0){
-                    cout<<0<<" ";
-                }else{
-                    cout<<i<<" ";
+    cin >> n;
+    int i = 1;
+    int count = 0;
+    while (i <= n) {
+        int tmp = i;
+        if (i / 10 != 0) {
+            while (tmp > 0) {
+                if (tmp / 10 == 3 || tmp / 10 == 6 || tmp / 10 == 9) {
+                    count = 1;
+                    break;
                 }
+                else if (tmp % 10 == 3 || tmp % 10 == 6 || tmp % 10 == 9 || i % 3 == 0) {
+                    count = 1;
+                    break;
+                }
+                else {
+                    count = 0;
+                }
+                tmp /= 10;
             }
-            
-        }else{
-            if(i == 3 || i == 6 || i == 9 || i%3==0){
-                cout<<0<<" ";
-            }else{
-                cout<<i<<" ";
+            if (count == 1) {
+                cout << 0 << " ";
+            }
+            else {
+                cout << i << " ";
             }
         }
-        
+        else {
+            if (i == 3 || i == 6 || i == 9 || i % 3 == 0) {
+                cout << 0 << " ";
+            }
+            else {
+                cout << i << " ";
+            }
+        }
         i++;
     }
     return 0;
