185a068b6035867896c5d6324adba4c2137b1fc3
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-07 13:09:15 +0900

    [Passed] 사각형의 넓이 계속 계산하기 (2ms, 0MB)

---
+++ trail1/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int h,w;
+    char c;
+    while(c !='C'){
+        cin>>h>>w>>c;
+        cout<<h*w<<endl;
+    }
+    return 0;
+}
