f5d7ea410a805be49a7e006396493aff1bdcd3ef
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-19 09:24:15 +0900

    [Passed] 여러가지 진수변환 (2ms, 0MB)

---
+++ trail2/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N, B;
+    cin>>N>>B;
+
+    int digits[20] = {};
+    int cnt = 0;
+    if(B == 4){
+        while(true){
+            if(N < 4){
+                digits[cnt++] = N;
+                break;
+            }
+            digits[cnt++] = N % 4;
+            N /= 4;
+        }
+    }else if(B == 8){
+        while(true){
+            if(N < 8){
+                digits[cnt++] = N;
+                break;
+            }
+            digits[cnt++] = N % 8;
+            N /= 8;
+        }
+    }
+    
+    for(int i = cnt - 1; i >= 0; i--){
+        cout<<digits[i];
+    }
+
+    return 0;
+}
