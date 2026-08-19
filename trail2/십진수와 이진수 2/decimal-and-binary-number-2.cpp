92662709be61b15c06287ff8ea707e9bda0e250f
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-19 09:35:21 +0900

    [Passed] 십진수와 이진수 2 (2ms, 0MB)

---
+++ trail2/십진수와 이진수 2/decimal-and-binary-number-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string binary;
+    cin>>binary;
+    int num = 0;
+    for(int i = 0; i < binary.length(); i++){
+        num = num * 2 + (binary[i] - '0');
+    }
+  
+    num *= 17;
+
+    int digits[20] = {};
+    int cnt = 0;
+    while(true){
+        if(num < 2){
+            digits[cnt++] = num;
+            break;
+        }
+
+        digits[cnt++] = num % 2;
+        num /= 2;
+    }
+
+   for(int i = cnt - 1; i >= 0; i--){
+        cout<<digits[i];
+   }
+    return 0;
+}
