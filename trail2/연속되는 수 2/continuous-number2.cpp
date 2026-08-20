20a5aedd4be99b65a5019ac9b19fe5535a914a28
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-20 18:04:46 +0900

    [Passed] 연속되는 수 2 (2ms, 0MB)

---
+++ trail2/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int cnt = 1;
+    int result = 1;
+
+    for (int i = 1; i < N; i++) {
+        if (arr[i] == arr[i - 1]) {
+            cnt++;
+        }else {
+            cnt = 1;
+        }
+
+        if (result < cnt) {
+            result = cnt;
+        }
+    }
+
+    cout << result;
+    return 0;
+}
