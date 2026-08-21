125df25bb62629ccf5cef716113a1cd3cc17b9fb
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-22 08:12:36 +0900

    [Passed] 씨 오 더블유 2 (2ms, 0MB)

---
+++ trail2/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string str;
+
+int main() {
+    cin >> n;
+    cin >> str;
+
+    // Please write your code here.
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            for(int k = j + 1; k < n; k++){
+                if(str[i] == 'C' && str[j] == 'O' && str[k] == 'W'){
+                    cnt++;
+                }
+           }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
