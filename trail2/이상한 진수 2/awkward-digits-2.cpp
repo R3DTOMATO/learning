45d41ce401ea645dd1754a3f560c4f4b5a8cbd1f
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-21 21:23:50 +0900

    [Passed] 이상한 진수 2 (1ms, 0MB)

--- trail2/이상한 진수 2/awkward-digits-2.cpp
+++ trail2/이상한 진수 2/awkward-digits-2.cpp
@@ -12,25 +12,16 @@ int main() {
     int max_decimal = 0;
     int decimal = 0;
     for(int i = 0; i < a.size(); i++){
-        if(a[i] == '0'){
-            a[i] = '1';
-            temp = '0';
-        }else if(a[i] == '1'){
-            a[i] = '0';
-            temp = '1';
-        }
-        int binary[a.size()] = {};
-        for(int j = 0; j < a.size(); j++){
-            binary[j] = a[j] - 48;
-        }
+        a[i] = '0' + '1' - a[i];
+
         decimal = 0;
         for(int j = 0; j < a.size(); j++){
-            decimal = decimal * 2 + binary[j];
+            decimal = decimal * 2 + (a[j] - '0');
         }
         if(max_decimal < decimal){
             max_decimal = decimal;
         }
-        a[i] = temp;
+        a[i] = '0' + '1' - a[i];
     }
 
     cout<<max_decimal;
