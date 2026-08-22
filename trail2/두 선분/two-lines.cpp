3808aaa5eedb3403fdf5df07421ccbaabd468942
R3DTOMATO <61935313+R3DTOMATO@users.noreply.github.com>
2026-08-22 12:09:28 +0900

    [Passed] 두 선분 (2ms, 0MB)

---
+++ trail2/두 선분/two-lines.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int x1, x2, x3, x4;
+
+int main() {
+    cin >> x1 >> x2 >> x3 >> x4;
+
+    // Please write your code here.
+    if(x2 < x3 || x1 > x4){
+        cout<<"nonintersecting";
+        
+    }else{
+        cout<<"intersecting";
+    }
+    return 0;
+}
